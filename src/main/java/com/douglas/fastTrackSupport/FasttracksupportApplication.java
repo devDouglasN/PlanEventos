612665ecac0e864afd5957b8a0eea8a9df61828f
Douglas <douglas_timaofiel@hotmail.com>
2023-09-09 16:25:13 -0300

    Initial commit

---
+++ src/main/java/com/douglas/fastTrackSupport/FasttracksupportApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.fastTrackSupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasttracksupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasttracksupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglas/fastTrackSupport/FasttracksupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.fastTrackSupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasttracksupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
