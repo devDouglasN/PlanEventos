5e30ac3e4518f0746d38e22ab6739969b2731d9d
Douglas <douglas_timaofiel@hotmail.com>
2023-09-11 14:30:17 -0300

    Padrão DTO para Técnico

--- src/main/java/com/douglas/fastTrackSupport/resources/TecnicoResource.java
+++ src/main/java/com/douglas/fastTrackSupport/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douglas.fastTrackSupport.domain.Tecnico;
+import com.douglas.fastTrackSupport.domain.TecnicoDTO;
 import com.douglas.fastTrackSupport.services.TecnicoService;
 
 @RestController
@@ -18,8 +19,8 @@ public class TecnicoResource {
 	private TecnicoService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }
