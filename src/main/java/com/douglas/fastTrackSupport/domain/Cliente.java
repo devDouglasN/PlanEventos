935a28fc6b6fab0e6d7708deb8ab61639f8d0aac
Douglas <douglas_timaofiel@hotmail.com>
2023-09-11 12:27:34 -0300

    Prevenção de serialização em classes

--- src/main/java/com/douglas/fastTrackSupport/domain/Cliente.java
+++ src/main/java/com/douglas/fastTrackSupport/domain/Cliente.java
@@ -3,6 +3,8 @@ package com.douglas.fastTrackSupport.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.OneToMany;
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
