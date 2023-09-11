ca8077401f56fd346dddaf6cb67a4d9fcbda6446
Douglas <douglas_timaofiel@hotmail.com>
2023-09-11 18:02:37 -0300

    Endpoint findAll para Técnico

--- src/main/java/com/douglas/fastTrackSupport/services/TecnicoService.java
+++ src/main/java/com/douglas/fastTrackSupport/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.douglas.fastTrackSupport.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 }
