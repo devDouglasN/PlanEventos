a38f313e90409703e9177a3179167d69e9b9591d
Douglas <douglas_timaofiel@hotmail.com>
2023-09-11 12:28:43 -0300

    Endpoint GET para recuperar Técnico por ID

---
+++ src/main/java/com/douglas/fastTrackSupport/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.douglas.fastTrackSupport.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douglas.fastTrackSupport.domain.Tecnico;
+import com.douglas.fastTrackSupport.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	private TecnicoRepository repository;
+
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
