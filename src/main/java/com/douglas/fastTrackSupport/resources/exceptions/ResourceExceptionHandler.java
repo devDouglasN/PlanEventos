8b299d3e430693a0c6f984d0208a3fdf43415ddd
Douglas <douglas_timaofiel@hotmail.com>
2023-09-12 10:19:30 -0300

    Exceção para CPF e EMAIL já cadastrados

--- src/main/java/com/douglas/fastTrackSupport/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/douglas/fastTrackSupport/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.douglas.fastTrackSupport.resources.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,6 +21,16 @@ public class ResourceExceptionHandler {
 				"Object Not Found", ex.getMessage(), request.getRequestURI());
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			HttpServletRequest request) {
+
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Violação de dados", ex.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
 
--- src/main/java/com/douglas/fastTrackSupport/services/TecnicoService.java
+++ src/main/java/com/douglas/fastTrackSupport/services/TecnicoService.java
@@ -4,17 +4,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.douglas.fastTrackSupport.domain.Pessoa;
 import com.douglas.fastTrackSupport.domain.Tecnico;
 import com.douglas.fastTrackSupport.domain.TecnicoDTO;
+import com.douglas.fastTrackSupport.repositories.PessoaRepository;
 import com.douglas.fastTrackSupport.repositories.TecnicoRepository;
 
 @Service
 public class TecnicoService {
-	
+
 	@Autowired
 	private TecnicoRepository repository;
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -27,7 +33,20 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
 	}
 }
