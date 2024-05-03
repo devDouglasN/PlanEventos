1bcd64effba0c9c19797a7ed3602bfebc71c3681
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-03 10:34:30 -0300

    Carregando dados do usuário

---
+++ src/main/java/com/douglas/planeventos/services/UserDetailsServicelmpl.java
@@ -0,0 +1,29 @@
+package com.douglas.planeventos.services;
+
+import com.douglas.planeventos.domain.Pessoa;
+import com.douglas.planeventos.repositories.PessoaRepository;
+import com.douglas.planeventos.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class UserDetailsServicelmpl  implements UserDetailsService{
+
+	@Autowired
+	private PessoaRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Pessoa> user = repository.findByEmail(email);
+		if(user.isPresent()) {
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getSenha(), user.get().getPerfis());
+		}
+		throw new UsernameNotFoundException("Usuário não encontrado com e-mail: " + email);
+	}
+}
