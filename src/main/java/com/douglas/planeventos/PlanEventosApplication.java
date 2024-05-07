bbaa55d81660b0bc417d509aeafde30324d699ae
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-07 20:02:44 -0300

    Altera nome da classe principal

--- src/main/java/com/douglas/planeventos/PlanEventosApplication.java
+++ src/main/java/com/douglas/planeventos/PlanEventosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FasttracksupportApplication {
+public class PlanEventosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FasttracksupportApplication.class, args);
+		SpringApplication.run(PlanEventosApplication.class, args);
 	}
 
 }
--- src/test/java/com/douglas/planeventos/PlanEventosApplicationTests.java
+++ src/test/java/com/douglas/planeventos/PlanEventosApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FasttracksupportApplicationTests {
+class PlanEventosApplicationTests {
 
 	@Test
 	void contextLoads() {
