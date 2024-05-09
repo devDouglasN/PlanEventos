c60068cf8515f43f36825644c10ed52cb5a3aab4
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-09 14:54:28 -0300

    Validacao para horario do evento

---
+++ src/main/java/com/douglas/planeventos/evento/validadores/HorarioEventoRule.java
@@ -0,0 +1,18 @@
+package com.douglas.planeventos.evento.validadores;
+
+import jakarta.validation.ValidationException;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Component
+public class HorarioEventoRule {
+
+    public void validador(EventoDados dados) {
+        if (dados.horarioInicio().isBefore(LocalTime.of(19, 0)) ||
+                dados.horarioFim().isAfter(LocalTime.of(3, 0))) {
+            throw new ValidationException("O evento deve ocorrer entre 19h e 3h");
+        }
+    }
+}
