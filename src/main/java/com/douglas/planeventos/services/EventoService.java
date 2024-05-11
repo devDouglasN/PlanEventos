7c38251a280da15f2089461a768f24713ab7166e
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-11 13:03:40 -0300

    Conserto nos metodos de servico

--- src/main/java/com/douglas/planeventos/services/EventoService.java
+++ src/main/java/com/douglas/planeventos/services/EventoService.java
@@ -1,6 +1,8 @@
 package com.douglas.planeventos.services;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,7 +12,10 @@ import com.douglas.planeventos.domain.Participante;
 import com.douglas.planeventos.domain.dtos.EventoDTO;
 import com.douglas.planeventos.enums.HorarioEvento;
 import com.douglas.planeventos.enums.StatusEvento;
+import com.douglas.planeventos.evento.validadores.EventoDados;
+import com.douglas.planeventos.evento.validadores.ValidadorParaEvento;
 import com.douglas.planeventos.repositories.EventoRepository;
+import com.douglas.planeventos.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import jakarta.validation.Valid;
@@ -28,6 +33,9 @@ public class EventoService {
 	@Autowired
 	private ParticipanteService participanteService;
 
+	@Autowired
+	private List<ValidadorParaEvento> validadores;
+
 	public Evento findById(Integer id) {
 		Optional<Evento> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
@@ -37,36 +45,43 @@ public class EventoService {
 		return repository.findAll();
 	}
 
-	public Evento create(@Valid EventoDTO obj) {
-		return repository.save(newEvento(obj));
-	}
-
 	public Evento update(Integer id, @Valid EventoDTO objDTO) {
 		objDTO.setId(id);
 		Evento oldObj = findById(id);
-		oldObj = newEvento(objDTO);
+
+		EventoDados eventoDados = new EventoDados(
+				objDTO.getParticipante(),
+				objDTO.getOrganizador(),
+				objDTO.getDataEvento(),
+				objDTO.getHorarioInicio(),
+				objDTO.getHorarioFim(),
+				LocalDateTime.now(), // ou outra data que você queira usar
+				null // ou a quantidade de pessoas que você queira usar
+		);
+
+		oldObj = newEvento(eventoDados);
 		return repository.save(oldObj);
 	}
 
-	private Evento newEvento(EventoDTO obj) {
-		Organizador organizador = organizadorService.findById(obj.getOrganizador());
-		Participante participante = participanteService.findById(obj.getParticipante());
 
+	public Evento newEvento(EventoDados eventoDados) {
+
+		Organizador organizador = organizadorService.findById(eventoDados.idOrganizador());
+		Participante participante = participanteService.findById(eventoDados.idParticipante());
+
+		// Criação do evento
 		Evento evento = new Evento();
-		if (obj.getId() != null) {
-			evento.setId(obj.getId());
-		}
-		
-		if(obj.getStatus().equals(2)) {
-			evento.setDataEvento(LocalDate.now());
-		}
-
+		evento.setDataEvento(eventoDados.dataEvento());
+		evento.setHorarioInicio(eventoDados.horarioInicio());
+		evento.setHorarioFim(eventoDados.horarioFim());
 		evento.setOrganizador(organizador);
 		evento.setParticipante(participante);
-		evento.setStatus(StatusEvento.toEnum(obj.getStatus()));
-		evento.setHorario(HorarioEvento.toEnum(obj.getHorario()));
-		evento.setDescricao(obj.getDescricao());
-		evento.setLocal(obj.getLocal());
-		return evento;
+
+		validadores.forEach(validator -> validator.validador(eventoDados));
+
+		// Salvar o evento no repositório
+		return repository.save(evento);
 	}
+
+
 }
