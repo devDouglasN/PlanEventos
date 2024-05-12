cdc6e72b8f9918f7a99c0240ac5efa5876ec50f2
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-12 14:10:12 -0300

    Mapeamento para varias tabelas

--- src/main/java/com/douglas/planeventos/domain/Pessoa.java
+++ src/main/java/com/douglas/planeventos/domain/Pessoa.java
@@ -7,21 +7,14 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.douglas.planeventos.enums.Perfil;
+import jakarta.persistence.*;
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Pessoa implements Serializable{
 	private static final long serialVersionUID = 1L;
 
