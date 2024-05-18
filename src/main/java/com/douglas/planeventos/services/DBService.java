9aaaf97da52eb01ead24f51a20b722dfa3bf9163
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-18 18:22:44 -0300

    Conserto na ordem de instancia de objetos

--- src/main/java/com/douglas/planeventos/services/DBService.java
+++ src/main/java/com/douglas/planeventos/services/DBService.java
@@ -1,7 +1,9 @@
 package com.douglas.planeventos.services;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.douglas.planeventos.domain.Evento;
 import com.douglas.planeventos.domain.Organizador;
@@ -30,27 +32,58 @@ public class DBService {
 
 	public void startDB() {
 
-		Organizador org1 = new Organizador(null, "Douglas Nascimento", "173.500.000-06", "douglas@mail.com", encoder.encode("123"));
+		Evento e1 = new Evento();
+		e1.setDataEvento(LocalDate.now());
+		e1.setLocal("Local 1");
+		e1.setDescricao("Descrição evento 1");
+		e1.setStatus(StatusEvento.ABERTO);
+		e1.setHorario(HorarioEvento.NOITE);
+
+		List<Organizador> organizadores = new ArrayList<>();
+		List<Participante> participantes = new ArrayList<>();
+
+
+		Organizador org1 = new Organizador(null, "Douglas Nascimento", "550.458.110-96", "douglass@mail.com", encoder.encode("123"));
 		org1.addPerfil(Perfil.ADMIN);
-		Organizador org2 = new Organizador(null, "Clara Pereira", "937.131.780-97", "clara@mail.com", encoder.encode("123"));
-		Organizador org3 = new Organizador(null, "Guilherme Souza", "339.813.380-06", "guilherme@mail.com", encoder.encode("123"));
-		Organizador org4 = new Organizador(null, "Rafael Costa", "654.317.230-49", "rafael@mail.com", encoder.encode("123"));
-		Organizador org5 = new Organizador(null, "Carolina Lima", "413.020.050-06", "linus@mail.com", encoder.encode("123"));
+		organizadores.add(org1);
 
-		Participante par1 = new Participante(null, "Mahatma Gandhi", "844.940.030-93", "mahatma@mail.com", encoder.encode("123"));
-		Participante par2 = new Participante(null, "Frida Kahlo", "815.398.940-53", "mFrida@mail.com", encoder.encode("123"));
-		Participante par3 = new Participante(null, "Michael Jordan", "847.895.340-06", "michael@mail.com", encoder.encode("123"));
-		Participante par4 = new Participante(null, "Nelson Mandela", "741.487.910-03", "nelson@mail.com", encoder.encode("123"));
-		Participante par5 = new Participante(null, "Elon Musk", "161.633.540-87", "elon@mail.com", encoder.encode("123"));
+		Organizador org2 = new Organizador(null, "Clara Pereira", "349.035.750-71", "claraP@mail.com", encoder.encode("123"));
+		organizadores.add(org2);
 
-		Evento e1 = new Evento(null, LocalDate.now(), "Local 1", "Descrição evento 1", StatusEvento.ABERTO, HorarioEvento.MANHA, org1, par1);
-		Evento e2 = new Evento(null, LocalDate.now(), "Local 2", "Descrição evento 2", StatusEvento.ABERTO, HorarioEvento.TARDE, org1, par2);
-		Evento e3 = new Evento(null, LocalDate.now(), "Local 3", "Descrição evento 3", StatusEvento.ENCERRADO, HorarioEvento.NOITE, org2, par3);
-		Evento e4 = new Evento(null, LocalDate.now(), "Local 4", "Descrição evento 4", StatusEvento.ABERTO, HorarioEvento.MANHA, org3, par3);
-		Evento e5 = new Evento(null, LocalDate.now(), "Local 5", "Descrição evento 5", StatusEvento.ANDAMENTO, HorarioEvento.TARDE, org2, par1);
-		Evento e6 = new Evento(null, LocalDate.now(), "Local 6", "Descrição evento 6", StatusEvento.ANDAMENTO, HorarioEvento.NOITE, org1, par5);
+		Organizador org3 = new Organizador(null, "Guilherme Souza", "637.091.100-33", "guilhermeS@mail.com", encoder.encode("123"));
+		organizadores.add(org3);
 
-		pessoaRepository.saveAll(Arrays.asList(org1, org2, org3, org4, org5, par1, par2, par3, par4, par5));
-		eventoRepository.saveAll(Arrays.asList(e1, e2, e3, e4, e5, e6));
+		Organizador org4 = new Organizador(null, "Rafael Costa", "457.396.390-18", "rafaelC@mail.com", encoder.encode("123"));
+		organizadores.add(org4);
+
+		Organizador org5 = new Organizador(null, "Carolina Lima", "715.216.650-10", "carolL@mail.com", encoder.encode("123"));
+		organizadores.add(org5);
+
+		// Adição dos participantes
+		Participante par1 = new Participante(null, "Mahatma Gandhi", "260.088.730-09", "mahatmaG@mail.com", encoder.encode("123"));
+		participantes.add(par1);
+
+		Participante par2 = new Participante(null, "Frida Kahlo", "403.273.480-11", "mFridaK@mail.com", encoder.encode("123"));
+		participantes.add(par2);
+
+		Participante par3 = new Participante(null, "Michael Jordan", "349.513.400-03", "michaelJ@mail.com", encoder.encode("123"));
+		participantes.add(par3);
+
+		Participante par4 = new Participante(null, "Nelson Mandela", "708.497.120-33", "nelsonM@mail.com", encoder.encode("123"));
+		participantes.add(par4);
+
+		Participante par5 = new Participante(null, "Elon Musk", "600.590.530-97", "elonM@mail.com", encoder.encode("123"));
+		participantes.add(par5);
+
+		// Adição das listas de organizadores e participantes ao evento
+		e1.setOrganizadores(organizadores);
+		e1.setParticipantes(participantes);
+
+		// Salva os organizadores e participantes
+		pessoaRepository.saveAll(organizadores);
+		pessoaRepository.saveAll(participantes);
+
+		// Salva o evento
+		eventoRepository.save(e1);
 	}
 }
